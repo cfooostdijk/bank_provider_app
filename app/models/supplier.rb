47361b15838707a12f1b9dac58c5fa9f9f0592f7
CrisOos <cristian.oostdijk@avenida.com>
2024-01-07 17:24:49 -0300

    TDD Supplier Model(Pass)

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,9 @@
 class Supplier < ApplicationRecord
   belongs_to :bank
+
+  validates :nit, format: { with: /\A([0-9]{9}-[0-9]{1})?\z/ }, allow_blank: false
+  validates :contact_phone, length: { maximum: 10 }, allow_blank: true
+  validates :account_number, length: { maximum: 15 }, allow_blank: true
+
+  validates_presence_of :name, :contact_name, :nit
 end
