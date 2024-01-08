a2117142ed5049c3aa0a7c383129289cb6b47551
CrisOos <cristian.oostdijk@avenida.com>
2024-01-08 11:32:59 -0300

    Devise functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'home#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  devise_scope :user do
+    get '/custom_sign_out', to: 'devise/sessions#destroy', as: :custom_destroy_user_session
+  end
+
 	resources :banks
   resources :dashboards
   resources :suppliers do
